5febd1fa149dd66cca180ee1c8d4bde4e5c84e6f
Manuel Fernando Jimenez Zamora <Manuel.Jimenez@bizagi.com>
2018-08-15 20:09:01 -0500

    Se agrega contador de favoritos en dropdown profile en el menu superior.

--- src/app/core/containers/top-nav-bar/top-nav-bar.component.ts
+++ src/app/core/containers/top-nav-bar/top-nav-bar.component.ts
@@ -5,6 +5,7 @@ import * as layout from "../../actions/layout";
 import { BookListService } from "../../../books-main/services/list/book-list.service";
 import { User } from "firebase/app";
 import {AngularFireAuth} from "angularfire2/auth";
+import {FavoritesService} from "../../../favorites/services/favorites.service";
 
 @Component({
   selector: 'app-top-nav-bar',
@@ -15,8 +16,9 @@ export class TopNavBarComponent implements OnInit {
 
   state: string;
   user: User;
+  countFavorites : number = 0;
 
-  constructor(private bookService: BookListService, private store: Store<fromRoot.State>, private authFire: AngularFireAuth) {
+  constructor(private bookService: BookListService, private store: Store<fromRoot.State>, private authFire: AngularFireAuth,private bookfavoritesService: FavoritesService) {
     this.state = 'open';
   }
 
@@ -25,6 +27,11 @@ export class TopNavBarComponent implements OnInit {
       .subscribe(
         user => {
           this.user = user;
+          this.bookfavoritesService.listFavorites(user).valueChanges().subscribe(
+            favoritesBooks => {
+                this.countFavorites = favoritesBooks.length;
+            }
+          );
         }
       )
   }
